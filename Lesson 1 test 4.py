feea63edbd04f756391be3c17a6a3c5eb1c2e13b
LeonidKhlynov <leonidkhlynov@yandex.ru>
2017-03-21 19:09:48 +0500

    Update 0.1

--- Lesson 1 test 4.py
+++ Lesson 1 test 4.py
@@ -1,8 +1,8 @@
 print("Enter 4 numbers")
-a = int(input("Enter number 1: "))
-b = int(input("Enter number 2: "))
-c = int(input("Enter number 3: "))
-d = int(input("Enter number 4: "))
+a = int(input("Enter number A: "))
+b = int(input("Enter number B: "))
+c = float(input("Enter number C: "))
+d = float(input("Enter number D: "))
 for i in range (a,b):
-	if (i/d==c):
-		print("Number: ",i)
+	if (i % d == c):
+		print("Number: ", i)
